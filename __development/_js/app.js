195cba2770b36e8e6b4e780fc294d66c726f20ac
y-sano <mmyy_125115@gmail.com>
2020-12-07 02:57:56 +0900

    スクロール処理関係

--- __development/_js/app.js
+++ __development/_js/app.js
@@ -1,1 +1,70 @@
-console.log("js");
+/**
+ * import
+ */
+require('intersection-observer');
+require('smoothscroll-polyfill');
+
+
+/**
+ * 読み込み後
+ */
+window.addEventListener('load',function(){
+
+    // intersect area
+    const options = {
+        root: null,
+        rootMargin: "-30px",
+        threshold: 0
+    };
+    const observer = new IntersectionObserver(doWhenIntersect, options);
+    const observer_target = document.querySelectorAll('.right-sections');
+
+    observer_target.forEach(e=>{
+        observer.observe(e);
+    });
+
+
+    const click_target = document.querySelectorAll('.ank');
+    click_target.forEach(click_target=>{
+
+        click_target.addEventListener('click',function(e){
+            const scroll_target = document.querySelectorAll('.right-title');
+            let target_attribute = '';
+            scroll_target.forEach(element=>{
+                target_attribute = element.getAttribute('category');
+                if(target_attribute === e.target.getAttribute('category')){
+                    element.scrollIntoView({
+                        behavior: 'smooth',
+                        block: 'start'
+                    });
+                }
+            });
+        });
+
+    });
+
+});
+
+
+/**
+ *
+ * @param {*} intersectfunction
+ */
+const doWhenIntersect = target => {
+
+    target.forEach(entry=>{
+
+        if(entry.isIntersecting){
+            const currentActive = document.querySelectorAll('.anks .current');
+            if(currentActive !== null){
+                currentActive[0].classList.remove('current');
+            }
+
+            const currentAttribute = entry.target.getAttribute('category');
+            const isActive = document.querySelector('li[category="' + currentAttribute + '"]');
+            isActive.classList.add('current');
+
+        }
+    });
+
+}
