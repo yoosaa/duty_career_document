f6059b0282f4a46bfcd23b8b18c352c44a841764
y-sano <mmyy_125115@gmail.com>
2020-12-06 23:26:42 +0900

    css 整理

--- __development/webpack.config.js
+++ __development/webpack.config.js
@@ -3,12 +3,34 @@ const path = require('path');
 module.exports = {
   mode: 'production',
   // エントリーポイント
-  entry: './js/app.js',
+  entry: './_js/app.js',
   // 出力
   output: {
     // ファイル名
     filename: 'app_bundle.js',
     // 出力先
     path: path.join(__dirname, '../')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',   //loader名
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  useBuiltIns: 'usage',
+                  corejs: 3,
+                },
+              ],
+            ],
+          }
+        }
+      }
+    ]
   }
 };
